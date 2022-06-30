54fc08ce5719ed843fc2014d66edfe6375043aba
Roberto Bonafiglia <roberto.bonafiglia@suse.com>
2022-06-30 15:17:03 +0200

    Updates docker to use larynx as tts

--- internal/tts/tts.go
+++ internal/tts/tts.go
@@ -1,6 +1,14 @@
 // Package tts is reponsible for reading out loud the given text
 package tts
 
+import (
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/jimmykarily/open-ocr-reader/internal/logger"
+)
+
 type TTS interface {
 	Speak(text string) error
 }
@@ -12,5 +20,28 @@ func NewDefaultTTS() DefaultTTS {
 }
 
 func (t DefaultTTS) Speak(text string) error {
+	logger := logger.New()
+	f, err1 := os.Create("output.wav")
+	if err1 != nil {
+		return err1
+	}
+	defer f.Close()
+	larynxCmd := exec.Command("larynx", "--raw-stream", text)
+	audioStream, _ := larynxCmd.StdoutPipe()
+	larynxCmd.Start()
+	bytesRead := make([]byte, 1000)
+	n, err := audioStream.Read(bytesRead)
+	for ; err == nil; n, err = audioStream.Read(bytesRead) {
+		_, err = f.Write(bytesRead[:n])
+		if err != nil {
+			return err
+		}
+	}
+	if err != io.EOF {
+		return err
+	}
+	// The generated stream is a stream raw 16-bit 22050Hz mono PCM audio to play it use cat output.wav | aplay -r 22050 -c 1 -f S16_LE
+	logger.Log("End of the audio steam\n")
+	larynxCmd.Wait()
 	return nil
 }
