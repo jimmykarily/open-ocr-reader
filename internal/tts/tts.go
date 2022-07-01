15da23dad3f80e3d3e006e88e4eaf34fc1138db8
Roberto Bonafiglia <roberto.bonafiglia@suse.com>
2022-07-01 15:14:33 +0200

    Added error checks and text clean with regexp

--- internal/tts/tts.go
+++ internal/tts/tts.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"regexp"
 
 	"github.com/jimmykarily/open-ocr-reader/internal/logger"
 )
@@ -21,14 +22,26 @@ func NewDefaultTTS() DefaultTTS {
 
 func (t DefaultTTS) Speak(text string) error {
 	logger := logger.New()
+	var re = regexp.MustCompile(`![0-9A-Za-zÀ-ÖØ-öø-ÿ'".,:;\s\n]`)
+	text = re.ReplaceAllString(text, "")
 	f, err1 := os.Create("output.wav")
 	if err1 != nil {
 		return err1
 	}
 	defer f.Close()
 	larynxCmd := exec.Command("larynx", "--raw-stream", text)
-	audioStream, _ := larynxCmd.StdoutPipe()
-	larynxCmd.Start()
+	audioStream, err := larynxCmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	errStream, err := larynxCmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+	err = larynxCmd.Start()
+	if err != nil {
+		return err
+	}
 	bytesRead := make([]byte, 1000)
 	n, err := audioStream.Read(bytesRead)
 	for ; err == nil; n, err = audioStream.Read(bytesRead) {
@@ -42,6 +55,14 @@ func (t DefaultTTS) Speak(text string) error {
 	}
 	// The generated stream is a stream raw 16-bit 22050Hz mono PCM audio to play it use cat output.wav | aplay -r 22050 -c 1 -f S16_LE
 	logger.Log("End of the audio steam\n")
-	larynxCmd.Wait()
+	err = larynxCmd.Wait()
+	if err != nil {
+		errStr, err2 := io.ReadAll(errStream)
+		if err2 != nil {
+			return err2
+		}
+		logger.Log(string(errStr))
+		return err
+	}
 	return nil
 }
